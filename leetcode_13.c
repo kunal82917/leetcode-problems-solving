5e98f9363dae549182128a10de3bf4e183745d3a
Kunal Gupta <kunalgupta9340@gmail.com>
2026-09-19 21:36:06 +0530

    Implement romanToInt function to convert Roman numerals

---
+++ leetcode_13.c
@@ -0,0 +1,45 @@
+#include <string.h>
+
+int romanToInt(char* s) {
+    int total = 0;
+    int len = strlen(s);
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 'I' && s[i + 1] == 'V') {
+            total += 4;
+            i++; // skip next char
+        } else if (s[i] == 'I' && s[i + 1] == 'X') {
+            total += 9;
+            i++;
+        } else if (s[i] == 'X' && s[i + 1] == 'L') {
+            total += 40;
+            i++;
+        } else if (s[i] == 'X' && s[i + 1] == 'C') {
+            total += 90;
+            i++;
+        } else if (s[i] == 'C' && s[i + 1] == 'D') {
+            total += 400;
+            i++;
+        } else if (s[i] == 'C' && s[i + 1] == 'M') {
+            total += 900;
+            i++;
+        }
+        else if (s[i] == 'I') {
+            total += 1;
+        } else if (s[i] == 'V') {
+            total += 5;
+        } else if (s[i] == 'X') {
+            total += 10;
+        } else if (s[i] == 'L') {
+            total += 50;
+        } else if (s[i] == 'C') {
+            total += 100;
+        } else if (s[i] == 'D') {
+            total += 500;
+        } else if (s[i] == 'M') {
+            total += 1000;
+        }
+    }
+
+    return total;
+}
